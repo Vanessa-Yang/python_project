3e6eb1d85bfb7724776ba9a57060f469d4e01999
VanessaYang <yangjingru9040@163.com>
2024-05-15 16:52:40 +0800

    append: 图像模糊检测

--- util/image_processing.py
+++ util/image_processing.py
@@ -1,41 +1,49 @@
 import datetime
 import math
 import os
+import time
 
-import cv2 as cv
+import cv2
+import numpy as np
 from PIL import Image as PILImage, ImageFilter
 from PIL import ImageFont, ImageDraw, ImageEnhance
 from wand.image import Image
 
+BLURRY = "Blurry"
+NOT_BLURRY = "Not Blurry"
+
 
 # 彩色图像转灰度图像
 def color_to_grayscale():
-    img_color = cv.imread('C:\\Users\\Administrator\\Pictures\\images_for_test\\mini_img.png', cv.IMREAD_COLOR)
-    img_gray = cv.imread('C:\\Users\\Administrator\\Pictures\\images_for_test\\mini_img.png', cv.IMREAD_GRAYSCALE)
-    cv.imshow('grayscale', img_gray)
-    cv.imshow('color', img_color)
-    cv.waitKey(0)
-
-    # 创建灰度图片
-    img_gray_pil = PILImage.fromarray(img_gray)
-
+    start_time = time.time()
+    input_image = 'C:/Users/Administrator/Pictures/images_for_test/oyad_logo.png'
+    # 大图压缩
+    zip_image = compress_image_util(input_image, 300)
+    # 读取彩色图片
+    img_color = PILImage.open(zip_image)
+    # 如果图片的模式不是RGBA，则转换为RGBA模式
+    if img_color.mode != 'RGBA':
+        img_color = img_color.convert('RGBA')
+    # 创建一个白色背景图像
+    white_bg = PILImage.new('RGBA', img_color.size, (255, 255, 255))
+    # 将原始图片粘贴到白色背景上
+    white_bg.paste(img_color, (0, 0), img_color)
+    # 转换为灰度图像
+    img_gray = white_bg.convert('L')
     # 保存原始灰度图片
-    img_gray_pil.save("../images/original_gray.png")
+    img_gray.save("../images/original_gray.png")
 
     # 加透明度参数并保存不同透明度的图片
     for alpha in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-        transparent_img = img_gray_pil.copy().convert("RGBA")
-        datas = list(transparent_img.getdata())
-        new_data = []
-        for item in datas:
-            # 设置透明度参数
-            new_data.append((item[0], item[1], item[2], int(255 * alpha)))
-        transparent_img.putdata(new_data)
+        transparent_img = img_gray.copy()
+        transparent_img.putalpha(int(alpha * 255))
         transparent_img.save(f"../images/gray_with_alpha_{alpha}.png")
+    end_time = time.time()
+    print("耗时：", end_time - start_time)
 
 
 # 图像压缩
-def compress_image_util(image_path, resize_pix=700, resolution=300, quality=70, out_path='./data/images/'):
+def compress_image_util(image_path, resize_pix=700, resolution=300, quality=70, out_path='../images/'):
     img = Image(filename=image_path)
     time_stamp = str(int(datetime.datetime.now().timestamp() * 100000))
     img.resolution = resolution if min(img.resolution) > resolution else img.resolution
@@ -185,7 +193,96 @@ def test_add_watermark():
     print("水印添加完成")
 
 
+# 判断是否为模糊图像
+def blurry_detect(image_path, threshold=1500):
+    if threshold < 1:
+        threshold = 1500
+    # 灰度化
+    img = cv2.imread(image_path)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print(gray_img.shape)
+
+    # 计算灰度图的边缘方差
+    fm = laplacian_var(gray_img)
+    text = 'Not Blurry'
+
+    # 设置输出文字
+    if fm < threshold:
+        text = "Blurry"
+    else:
+        # 局部模糊检测
+        is_burry = local_blur_detect(fm, gray_img, img, threshold)
+        if is_burry:
+            text = "Local Blurry"
+
+    # 显示结果
+    cv2.putText(img, "{}: {:.2f}".format(text, fm), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyWindow('Image')
+
+
+# 局部模糊检测
+def local_blur_detect(fm, gray_img, img, threshold):
+    # 分四个区域再次检测，模糊区域两个及以上则视为模糊
+    blur_count = 0
+    sep_count = 2 if fm < 1000 else 3
+    w, h = gray_img.shape
+    mid_x, mid_y = int(w // 2), int(h // 2)
+    error_x, error_y = w % 2, h % 2
+    _img = img.copy()
+    img1 = _img[0:(mid_x + error_x), 0:(mid_y + error_y)]
+    img2 = _img[mid_x: w, 0:(mid_y + error_y)]
+    img3 = _img[0:(mid_x + error_x), mid_y: h]
+    img4 = _img[mid_x:w, mid_y: h]
+    fm1 = laplacian_var(cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY))
+    fm2 = laplacian_var(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY))
+    fm3 = laplacian_var(cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY))
+    fm4 = laplacian_var(cv2.cvtColor(img4, cv2.COLOR_BGR2GRAY))
+    blur_count += 1 if fm1 < threshold else 0
+    blur_count += 1 if fm2 < threshold else 0
+    blur_count += 1 if fm3 < threshold else 0
+    blur_count += 1 if fm4 < threshold else 0
+    local_text1 = BLURRY if fm1 < threshold else NOT_BLURRY
+    local_text2 = BLURRY if fm2 < threshold else NOT_BLURRY
+    local_text3 = BLURRY if fm3 < threshold else NOT_BLURRY
+    local_text4 = BLURRY if fm4 < threshold else NOT_BLURRY
+    # 展示多个
+    cv2.putText(img1, "{}: {:.2f}".format(local_text1, fm1), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255),
+                2)
+    cv2.putText(img2, "{}: {:.2f}".format(local_text2, fm2), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255),
+                2)
+    cv2.putText(img3, "{}: {:.2f}".format(local_text3, fm3), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255),
+                2)
+    cv2.putText(img4, "{}: {:.2f}".format(local_text4, fm4), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255),
+                2)
+    # 图集
+    imgs = np.hstack((img1, img2, img3, img4))
+    cv2.imshow('local_images_of_4', imgs)
+    cv2.waitKey(0)
+    cv2.destroyWindow('local_images_of_4')
+
+    return blur_count >= sep_count
+
+
+# 拉普拉斯变换，求取方差 —— 边缘检测值，值越大，认为图片越清晰；反之，越模糊
+def laplacian_var(gray_img):
+    # 拉普拉斯变换
+    gray_lap = cv2.Laplacian(gray_img, cv2.CV_64F)
+    dst = cv2.convertScaleAbs(gray_lap)
+    # 求取方差
+    fm = dst.var()
+    return fm
+
+
 if __name__ == '__main__':
     # test_compress_image()
     # test_add_watermark()
-    color_to_grayscale()
+    # color_to_grayscale()
+
+    # 遍历目录下每一张图片
+    folder_path = 'C:\\Users\\Administrator\\Pictures\\opencv'
+    for image_name in os.listdir(folder_path):
+        _image_path = os.path.join(folder_path, image_name)
+        print(_image_path)
+        blurry_detect(_image_path, 350)
